967c3cd87dfe6fd51c103e91818dcc5ccd54373f
Eric Dobbs <edobbs@newrelic.com>
2018-11-27 14:10:41 -0800

    Greet each exercise

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ type thing struct {
 }
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, Variables")
 	p := thing{
 		id:    "random",
 		title: "This Sentence No Verb",
@@ -17,5 +17,28 @@ func main() {
 	}
 
 	fmt.Printf("%#v\n", p)
-	fmt.Println("Hello again, World")
+
+	fmt.Println("Hello, Pointers")
+	pointy := 20
+	fmt.Printf("%#v\t%#v\n", pointy, &pointy)
+
+	pointypointy := &pointy
+	fmt.Printf("#v pointypointy: %#v\n", pointypointy)
+	fmt.Printf("address: %+v\nvalue: %+v\npoints:%+v\n", &pointypointy, pointypointy, *pointypointy)
+
+	fmt.Println("Hello, Control Flow")
+	name := "Barney"
+	age := 32
+
+	switch name {
+	case "George", "Marty":
+		fmt.Println("Hey, McFly!")
+	case "Fred", "Barney":
+		fmt.Println("do you want a Brontosaurus Burger?")
+	}
+
+	if age > 6 {
+		fmt.Println("Here's a cookie")
+	}
+
 }
